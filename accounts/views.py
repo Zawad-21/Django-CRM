cf66caac5a7ba7d74ffc6485982c45ad918def08
Zawad Ahmed <21zawadahmed21@gmail.com>
2021-08-02 00:08:22 +0600

    added django signals funtionality

--- accounts/views.py
+++ accounts/views.py
@@ -22,9 +22,6 @@ def registerPage(request):
         if form.is_valid():
             user = form.save()
             username = form.cleaned_data.get('username')
-            group = Group.objects.get(name='customer')
-            user.groups.add(group)
-            Customer.objects.create(user=user)
             messages.success(
                 request, 'Account successfully created ' + username)
             return redirect('login')
